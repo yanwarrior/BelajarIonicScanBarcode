7d60f74c7baec211e056935bb600dce361956d76
yanwarsolah <yanwarsolah@gmail.com>
2020-05-27 15:37:12 +0700

    Membuat Scanner Barcode dan QR di Ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    BarcodeScanner,
+    { 
+      provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy 
+    }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
 
+import {
+  BarcodeScannerOptions,
+  BarcodeScanner
+} from '@ionic-native/barcode-scanner/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  encodeData: any;
+  scannedData: {};
+  barcodeScannerOptions: BarcodeScannerOptions;
 
-  constructor() {}
+  constructor(
+    private barcodeScanner: BarcodeScanner
+  ) {
+    this.encodeData = "https://google.com";
+    this.barcodeScannerOptions = {
+      showFlipCameraButton: true,
+      showTorchButton: true
+    }
+  }
+
+  scanCode() {
+    this.barcodeScanner.scan()
+      .then(barcodeData => {
+        alert("Barcode data berisi: " 
+          + JSON.stringify(barcodeData));
+        this.scannedData = barcodeData;
+      })
+      .catch(error => {
+        console.log("Error", error);
+      })
+  }
+
+  encodedText() {
+    this.barcodeScanner.encode(
+      this.barcodeScanner.Encode.TEXT_TYPE,
+      this.encodeData
+    )
+    .then(
+      encodedData => {
+        console.log(encodedData);
+        this.encodeData = encodedData;
+      },
+      error => {
+        console.log("Error occured", error);
+      }
+    );
+  }
 
 }
